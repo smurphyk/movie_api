22600584fddcf300fff04da0eb88cf6eacd36c2d
Sean Murphy <smurphyk@DESKTOP-IFP8V84.localdomain>
2020-05-28 12:53:25 -0500

    Added mongoose function to get a user by username

--- index.js
+++ index.js
@@ -101,6 +101,18 @@ app.post('/users', (req, res) => {
     });
 });
 
+// Get a user by username
+app.get('/users/:Username', (req, res) => {
+  Users.findOne({ Username: req.params.Username })
+    .then((user) => {
+      res.json(user);
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).send('Error: ' + err);
+    });
+});
+
 // Allows user to upddate personal information
 app.put('/users/:username', (req, res) => {
   let user = users.find((user) => { return user.username === req.params.username });
