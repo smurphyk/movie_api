dcbff21a8351a262d7ea3dbf3e47928c3f3f0972
Sean Murphy <smurphyk@DESKTOP-IFP8V84.localdomain>
2020-05-29 16:06:33 -0500

    Added additional callback code to localStrategy in passport.js to validate any password entered by a user.

--- passport.js
+++ passport.js
@@ -20,7 +20,12 @@ passport.use(new LocalStrategy({
 
     if (!user) {
       console.log('incorrect username');
-      return callback(null, false, {message: 'Incorrect Username or Password.'});
+      return callback(null, false, {message: 'Incorrect Username.'});
+    }
+
+    if (!user.validatePassword(password)) {
+      console.log('incorrect password');
+      return callback(null, false, {message: 'Incorrect Password.'});
     }
 
     console.log('finished');
