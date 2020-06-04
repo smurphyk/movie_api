202600559a812584534c63ca5218185d0849d4c0
Sean Murphy <smurphyk@DESKTOP-IFP8V84.localdomain>
2020-06-04 11:18:19 -0500

    Created movie-card.jsx

--- client/dist/src.78399e21.js
+++ client/dist/src.78399e21.js
@@ -30300,7 +30300,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "64772" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "65253" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
