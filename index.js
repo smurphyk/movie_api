29d3e025170b2a4d0dfbb4003f1c6245ddbb4310
Sean Murphy <smurphyk@DESKTOP-IFP8V84.localdomain>
2020-06-04 11:23:09 -0500

    Removed CORs

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const Models = require('./models.js'),
   Movies = Models.Movie,
   Users = Models.User;
 
-var cors = require('cors');
+
 
 const { check, validationResult } = require('express-validator');
 
@@ -26,20 +26,7 @@ mongoose.connect(process.env.CONNECTION_URI, { useNewUrlParser: true, useUnified
 app.use(express.static('public'));
 app.use(morgan('common'));
 app.use(bodyParser.json());
-app.use(cors());
 
-app.use(cors({
-  origin: (origin, callback) => {
-    if (!origin) return callback(null, true);
-    if (allowedOrigins.indexOf(origin) !== -1) { // If a specific origin isn't found on the list of allowed allowedOrigins
-      let message = "The CORs policy for this application doesn't allow access from origin " + origin;
-      return callback(new Error(message), false);
-    }
-    return callback(null, true);
-  }
-}));
-
-var allowedOrigins = ['http://localhost:1234', '*'];
 
 // GET requests
 
