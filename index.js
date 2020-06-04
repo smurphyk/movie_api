05f7d288064c713e42007f4883c28a54821f7c7d
Sean Murphy <smurphyk@DESKTOP-IFP8V84.localdomain>
2020-06-04 10:00:03 -0500

    commit

--- index.js
+++ index.js
@@ -25,19 +25,7 @@ app.use(bodyParser.json());
 
 let auth = require('./auth')(app);
 
-/* const cors = require('cors');
-let allowedOrigins = ['http://localhost:1234', '*'];
 
- app.use(cors({
-  origin: (origin, callback) => {
-    if (!origin) return callback(null, true);
-    if (allowedOrigins.indexOf(origin) === -1) { // If a specific origin isn't found on the list of allowed allowedOrigins
-      let message = "The CORs policy for this application doesn't allow access from origin " + origin;
-      return callback(new Error(message), false);
-    }
-    return callback(null, true);
-  }
-})); */
 
 // GET requests
 
