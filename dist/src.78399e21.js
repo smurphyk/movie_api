228467127c1cab3885b047e72cd91d85b25897f0
Sean Murphy <smurphyk@DESKTOP-IFP8V84.localdomain>
2020-06-04 11:50:37 -0500

    fixed minor syntax errors

--- dist/src.78399e21.js
+++ dist/src.78399e21.js
@@ -30300,7 +30300,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "49999" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "50300" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
