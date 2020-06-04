213e5879d826c39a3aa2217bbffa4b99a8073d05
Sean Murphy <smurphyk@DESKTOP-IFP8V84.localdomain>
2020-06-04 12:17:05 -0500

    added onClick event to main-view and movie-card

--- client/src/components/main-view/main-view.jsx
+++ client/src/components/main-view/main-view.jsx
@@ -1,13 +1,18 @@
 import React from 'react';
 import axios from 'axios';
+
 import { MovieCard } from '../movie-card/movie-card';
+import { MovieView } from '../movie-view/movie-view';
 
 export class MainView extends React.Component {
   constructor() {
     super();
 
     // Initialize the state to an empty object so we can destructrue it later
-    this.state = {};
+    this.state = {
+      movies: null,
+      selectedMovie: null
+    };
   }
 
   componentDidMount() {
@@ -23,20 +28,29 @@ export class MainView extends React.Component {
       });
   }
 
+  onMovieClick(movie) {
+    this.setState({
+      selectedMovie: movie
+    });
+  }
+
   // this overrides the render() method of the superclass
   render() {
 
     // Before data is initially loaded
-    const { movies } = this.state;
+    const { movies, selectedMovie } = this.state;
 
     // Before movies have been loaded
     if (!movies) return <div className="main-view" />;
 
     return (
       <div className="main-view">
-        {movies.map(movie => (
-          <MovieCard key={movie.id} movie={movie} />
-        ))}
+        {selectedMovie
+          ? <MovieView movie={selectedMovie} />
+          : movies.map(movie => (
+            <MovieCard key={movie.id} movie={movie} onClick={movie => this.onMovieClick(movie)} />
+          ))
+        }
       </div>
     );
   }
