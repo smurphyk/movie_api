8546377fc69bc40191b68c7af49f10cd0b69bf4c
Sean Murphy <smurphyk@DESKTOP-IFP8V84.localdomain>
2020-06-08 16:43:23 -0500

    Created registration view for new users

---
+++ client/src/components/registration-view/registration-view.jsx
@@ -0,0 +1,80 @@
+import React, { useState } from 'react';
+import axios from 'axios';
+import { Link } from 'react-router-dom';
+import Container from 'react-bootstrap/Container';
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
+import './registration-view.scss';
+
+export function ResgistrationView(props) {
+  const [username, createUsername] = useState('');
+  const [password, createPassword] = useState('');
+  const [email, createEmail] = useState('');
+  const [birthday, createDob] = useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    axios
+      .post('https://murphmovies.herokuapp.com/users', {
+        Username: username,
+        Password: password,
+        Email: email,
+        Birthday: birthday,
+      })
+      .then((response) => {
+        const data = response.data;
+        alert("You now exist in the world of Murph's Movies! Please log in, if you dare!");
+        console.log(data);
+        window.open('/client', '_self');
+      })
+      .catch((error) => {
+        console.log('Swing and a miss! Error registering user');
+      });
+  };
+
+  return (
+    <Container className="registration-container">
+      <Form className="registration-form">
+        <Form.Group controlID="formBasicUsername">
+          <Form.Label>Username</Form.Label>
+          <Form.Control
+            type="text"
+            placeholder="Username"
+            value={username}
+            onChange={(e) => createUsername(e.target.value)} />
+        </Form.Group>
+        <Form.Group controlID="formBasicPassword">
+          <Form.Label>Password</Form.Label>
+          <Form.Control
+            type="password"
+            placeholder="Password"
+            value={password}
+            onChange={(e) => createPassword(e.target.value)} />
+        </Form.Group>
+        <Form.Group controlId="formBasicEmail">
+          <Form.Label>Email Address</Form.Label>
+          <Form.Control
+            type="email"
+            placeholder="Email Address"
+            value={email}
+            onChange={(e) => createEmail(e.target.value)} />
+          <Form.Text className="text-muted">
+            Your info is safe with us! We will never share with anyone, even your own mother.
+            </Form.Text>
+        </Form.Group>
+        <Form.Group controlID="formBasicDob">
+          <Form.Label>Birthday</Form.Label>
+          <Form.Control
+            type="date"
+            placeholder="mm/dd/yyyy"
+            value={birthday}
+            onChange={(e) => createDob(e.target.value)} />
+        </Form.Group>
+        <Button className="submit-user" type="submit" onClick={handleSubmit}>
+          Register
+        </Button>
+      </Form>
+    </Container>
+  );
+}
