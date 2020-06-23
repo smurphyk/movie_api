105ed2e9c6b0401a2ea9036e6d22bac61ea719dc
Sean Murphy <smurphyk@DESKTOP-IFP8V84.localdomain>
2020-06-23 12:08:20 -0500

    fixed Login issues

--- index.js
+++ index.js
@@ -11,7 +11,6 @@ const Movies = Models.Movie;
 const Users = Models.User;
 
 const cors = require('cors');
-app.use(cors());
 
 const { check, validationResult } = require('express-validator');
 
@@ -21,16 +20,16 @@ require('./passport');
 
 const auth = require('./auth')(app);
 
-//mongoose.connect('mongodb://localhost:27017/movie_apiDB', { useNewUrlParser: true, useUnifiedTopology: true });
-mongoose.connect(process.env.CONNECTION_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+
 
 // Middleware
 app.use(express.static('public'));
 app.use(morgan('common'));
 app.use(bodyParser.json());
+app.use(cors());
 
-
-
+//mongoose.connect('mongodb://localhost:27017/movie_apiDB', { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect(process.env.CONNECTION_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 
 // GET requests
 
