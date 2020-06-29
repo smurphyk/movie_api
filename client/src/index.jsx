1c5ac558437ad7fb247bbf33969558a89321a1d9
Sean Murphy <smurphyk@DESKTOP-IFP8V84.localdomain>
2020-06-29 10:18:22 -0500

    Created store in index.jsx file.

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,19 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { MainView } from './components/main-view/main-view';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
 
-// Import statement to indicate that you need to bundle `./index.scss`
+import { MainView } from './components/main-view/main-view';
+import moviesApp from './reducers/reducers';
+
 import './index.scss';
 
-// Main component (will eventrually use all the others)
+const store = creatStore(moviesApp);
+
 class MyFlixApplication extends React.Component {
   render() {
-    return <MainView />;
+    return (
+      <Provider store={store}>
+        <MainView />
+      </Provider>
+    );
   }
 }
 
-// Finds the root of your app
 const container = document.getElementsByClassName('app-container')[0];
 
-// Tells React to render your app in the rood DOM element
 ReactDOM.render(React.createElement(MyFlixApplication), container);
